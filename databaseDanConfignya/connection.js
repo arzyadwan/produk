54baaf37a94fa72afe17ef2b1e9fa9403e0a6c97
unknown <arzyadwan@gmail.com>
2024-11-25 20:47:00 +0700

    asdasd

--- databaseDanConfignya/connection.js
+++ databaseDanConfignya/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 //konfig koneksi ke db
 const dbConfig = {
-    host: "34.142.223.99",
+    host: "34.101.248.27",
     user: "andi",
     password: "Ari421@&!",
     database: "db_produk",
